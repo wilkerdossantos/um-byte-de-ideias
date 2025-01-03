09e0facf46be49a054d4bace8cfabbb5fad70de9
Wilker Santos <wilker.santos.ribeiro@gmail.com>
2025-01-03 20:10:02 -0300

    Include admin app tags

--- tags/admin.py
+++ tags/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Tag
 
-# Register your models here.
+@admin.register(Tag)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    prepopulated_fields = {'slug': ('name',)}
