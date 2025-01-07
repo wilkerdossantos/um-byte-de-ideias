f5bb69cd60b5d2c43044a1141ec637af7b1b589a
Wilker Santos <wilker.santos.ribeiro@gmail.com>
2025-01-07 14:39:32 -0300

    Fix Model Comments

--- comments/models.py
+++ comments/models.py
@@ -6,7 +6,7 @@ class Comment(models.Model):
     name = models.CharField(max_length=255) # Refatorar para UserName
     email = models.EmailField(max_length=255)
     comment = models.TextField(null=True, blank=True)
-    post = models.ForeignKey(Post, on_delete=models.PROTECT, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.PROTECT, related_name='comments', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
